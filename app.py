1b820a3efba01f184ac6c965efd96a4558cf978b
Ko Yamaura <k.kingdomail@gmail.com>
2018-10-18 00:00:55 +0900

    success to control LED

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
+import sys
+sys.path.append('../GrovePi/Software/Python/')
+from grovepi import *
 
 app = Flask(__name__)
-LEDpin = 4
+LED = 4
 LEDStatus = False
+
+#setup
+
+pinMode(LED, "OUTPUT")
 
 @app.route('/')
 def index():
@@ -13,14 +20,12 @@ def index():
 
 @app.route('/on')
 def on():
-    print "on"
-    # digitalWrite(LED, 1)
+    digitalWrite(LED, 1)
     LEDStatus = True
     return render_template('on.html')
 
 @app.route('/off')
 def off():
-    print "off"
     LEDStatus = False
-    # digitalWrite(LED, 1)
+    digitalWrite(LED, 0)
     return render_template('off.html')
